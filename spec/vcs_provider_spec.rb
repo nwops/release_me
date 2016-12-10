e634a860804bed851cf2593c5b198375e3916b20
Corey Osman <corey@nwops.io>
2016-12-09 19:09:13 -0800

    adds more tests

--- spec/vcs_provider_spec.rb
+++ spec/vcs_provider_spec.rb
@@ -5,9 +5,14 @@ describe ReleaseMe::VcsProvider do
   include described_class
   before :each do
     ENV['GITLAB_CI'] = 'true'
+    ENV['CI_PROJECT_URL'] = 'https://github.com/api/v3/projects/1'
+    ENV['CI_PROJECT_ID'] = '1'
+    ENV['PRIVATE_TOKEN'] = '1234'
   end
-
-  let(:file_path) do
+  let(:config) do
+    provider_config(project_path)
+  end
+  let(:project_path) do
     File.expand_path(File.join('spec', 'providers', 'puppet'))
   end
 
@@ -16,39 +21,50 @@ describe ReleaseMe::VcsProvider do
   end
 
   describe :gitlab do
-    let(:file_path) do
+    let(:project_path) do
       File.expand_path(File.join('spec', 'adapters', 'puppet'))
     end
     it 'returns provider config' do
-      expect(provider_config(file_path)['provider_name']).to eq('gitlab')
+      expect(config['provider_name']).to eq('gitlab')
     end
   end
 
   describe :gem do
-    let(:file_path) do
+    let(:project_path) do
       File.expand_path(File.join('spec', 'adapters', 'gem'))
     end
     it 'returns provider config' do
-      expect(provider_config(file_path)['provider_name']).to eq('gitlab')
+      expect(config['provider_name']).to eq('gitlab')
     end
   end
 
   describe :local do
-    before :each do
-      ENV['GITLAB_CI'] = nil
+    before :all do
+      ENV['GITLAB_CI'] = 'true'
+      ENV['CI_PROJECT_URL'] = 'https://github.com/api/v3/projects/1'
+      ENV['CI_PROJECT_ID'] = '1'
+      ENV['PRIVATE_TOKEN'] = '1234'
     end
-    let(:file_path) do
+    let(:project_path) do
       File.expand_path(File.join('spec', 'adapters', 'local'))
     end
     it 'returns gitlab provider config' do
-      expect(provider_config(file_path)['provider_name']).to eq('gitlab')
+      expect(config['provider_name']).to eq('gitlab')
     end
     it 'returns local config' do
-      expect(local_config(file_path)['vcs']['provider']).to eq('gitlab')
+      expect(config['provider']).to eq('gitlab')
     end
 
     it 'can override base_url' do
-      expect(provider_config(file_path)['base_url']).to eq('https://mygitlab.com/api/v3/projects')
+      expect(config['base_url']).to eq('https://mygitlab.com/api/v3/projects')
+    end
+
+    it 'can override author name' do
+      expect(config['author_name']).to eq('Autobot')
+    end
+
+    it 'can override author email' do
+      expect(config['author_email']).to eq('autobot@nwops.io')
     end
   end
 end
